7d53e880aefa20db0ca034b9faff457b43ce670a
iliyasbratan <i_yerzhanuly@kbtu.kz>
2025-02-02 21:42:16 +0500

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,40 @@
+print("Hello, Iliyas!")
+
+x = "Iliyas"
+print (x)
+
+# This is comment
+
+"""This is also a comment"""
+
+x = "John"
+# is the same as
+x = 'John'
+
+a = 5
+# is the not same as
+A = 5
+
+x = 5
+print(type(x))
+
+#convert from int to float:
+x = float(1)
+#convert from float to int:
+y = int(2.8)
+#convert from int to complex:
+z = complex(1)
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+a = "Hello, World!"
+print(a.upper())
